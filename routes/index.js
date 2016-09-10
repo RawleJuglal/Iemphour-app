0ff63b3f65d2eff1b3c8546087a35f9f1ed316f5
Rawle Juglal <rawledjuglal@gmail.com>
2016-09-10 04:21:02 +0000

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {  
+  res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res) {  
+  res.render('signup.ejs', { message: req.flash('loginMessage') });
+});
+
+router.get('/profile', isLoggedIn, function(req, res) {  
+  res.render('profile.ejs', { user: req.user });
+});
+
+router.get('/logout', function(req, res) {  
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
+
+function isLoggedIn(req, res, next) {  
+  if (req.isAuthenticated())
+      return next();
+  res.redirect('/');
+}
